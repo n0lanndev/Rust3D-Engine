ae4b35c02eb0b7ff89620c8efcae7ed263840f9a
Zeynn <zartox53@gmail.com>
2026-05-04 19:55:28 +0200

    Add Sphere colored according to its normal vectors

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@ use color::Color;
 mod ray;
 use ray::Ray;
 
-fn hit_sphere(center: Point3, radius: f64, ray: &Ray) -> bool
+fn hit_sphere(center: Point3, radius: f64, ray: &Ray) -> f64
 {
     let oc: Vec3 = center - *ray.origin();
     let a: f64 = dot(*ray.direction(),*ray.direction());
@@ -20,14 +20,24 @@ fn hit_sphere(center: Point3, radius: f64, ray: &Ray) -> bool
     let c: f64 = dot(oc, oc) - radius*radius;
     let discriminant: f64 = b*b - 4.0 * a * c;
 
-    discriminant >= 0.0
+    if discriminant < 0.0
+    {
+        return -1.0;
+    }
+    else
+    {
+        return (-b - discriminant.sqrt()) / (2.0 *a);
+    }
 
 }
 
 fn ray_color(r : Ray) -> Color{
-    if hit_sphere(Point3::new(0.0,0.0,-1.0), 0.5, &r)
+
+    let t : f64 = hit_sphere(Point3::new(0.0,0.0,-1.0), 0.5, &r);
+    if t > 0.0
     {
-        return Color::new(1.0, 0.0, 0.0);
+        let n : Vec3 = unit_vector(r.at(t) - Vec3::new(0.0, 0.0,-1.0));
+        return 0.5 * Color::new(n.x+1.0, n.y+1.0, n.z+1.0);
     }
     let unit_vector : Vec3 = unit_vector(*r.direction());
     let a : f64 = 0.5 * (unit_vector.y + 1.0);
